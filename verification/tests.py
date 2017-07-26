ad0e3231c22468af800730c78c00f5a07aa2a842
oleksandrliabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2017-07-27 00:01:04 +0300

    initial commit

---
+++ verification/tests.py
@@ -0,0 +1,61 @@
+"""
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
+    "answer" -- your right answer
+    "explanation" -- not necessary key, it's using for additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": "/a/",
+            "answer": "/a"
+        },{
+            "input": "/a/b",
+            "answer": "/a/b"
+        },{
+            "input": "/a//b/c",
+            "answer": "/a/b/c"
+        },{
+            "input": "/a/b/./ci",
+            "answer": "/a/b/ci"
+        },{
+            "input": "/a/b/../c",
+            "answer": "/a/c"
+        },{
+            "input": "/a/b//",
+            "answer": "/a/b"
+        },{
+            "input": "a/../b/c",
+            "answer": "b/c"
+        }
+        
+    ],
+    "Extra": [
+        {
+            "input": "/a/v/c",
+            "answer": "/a/v/c"
+        },{
+            "input": "/mix/max/../num/../vim",
+            "answer": "/mix/vim"
+        },{
+            "input": "/../a/big",
+            "answer": "/a/big"
+        },{
+            "input": "/../../v1//",
+            "answer": "/v1"
+        },{
+            "input": "fr/mi/././hy",
+            "answer": "fr/mi/hy"
+        },{
+            "input": "/ft/g.t/ft..gy/f..g..t/",
+            "answer": "/ft/g.t/ft..gy/f..g..t"
+        },{
+            "input": "/",
+            "answer": "/"
+        }
+    ]
+}
