fc9e769398fe28ca1405a4ef8b5163f9cd78321b
Stefan Pochmann <stefan.pochmann@gmail.com>
2017-08-01 00:45:53 +0200

    Added more tests.

--- verification/tests.py
+++ verification/tests.py
@@ -84,8 +84,17 @@ TESTS = {
             "input": "/../foo/../foo",
             "answer": "/foo"
         },{
+            "input": "///",
+            "answer": "/"
+        },{
+            "input": "///d",
+            "answer": "/d"
+        },{
             "input": "////",
             "answer": "/"
+        },{
+            "input": "////d",
+            "answer": "/d"
         },{
             "input": "//foo//goo",
             "answer": "/foo/goo"
